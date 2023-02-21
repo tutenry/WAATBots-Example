f68125232bc953b9c45a8d042e93912c49c9f320
Rylan <rytu10@icloud.com>
2023-02-21 18:46:01 -0500

    fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -31,7 +31,7 @@ public class RobotContainer {
 
     configureBindings();
   }
-
+//s
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
    * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
